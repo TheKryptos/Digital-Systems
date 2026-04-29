13824943348739f8d8bd0674928c11196b221806
TheKryptos <danialtana89@gmail.com>
2026-04-29 23:43:14 +0000

    BCD added

--- rtl/binary_to_bcd.sv
+++ rtl/binary_to_bcd.sv
@@ -1,1 +1,8 @@
-module
+module binary_to_bcd (
+    input logic [6:0] bin,   // binary inputs, 0-99
+    output logic [3:0] tens, // decimal tens digit (BCD)
+    output logic [3:0] ones  // decimal ones digit (BCD)
+);
+    assign tens = 4'(bin / 7'd10);
+    assign ones = 4'(bin % 7'd10);
+endmodule
